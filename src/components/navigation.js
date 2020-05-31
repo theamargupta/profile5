d2760593c6b39d988c09a1efb0957cf385fb8079
Amar Gupta <amar9911056196@gmail.com>
2020-06-01 01:01:53 +0530

    update

---
+++ src/components/navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navigation () {
+    return (
+    <nav className="site-navigation">
+        <span> My React Blog</span>
+        <ul>
+        <li>
+        
+        </li>
+        </ul>
+    </nav>)
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './assets/scss/base.scss'
 ReactDOM.render(
   <React.StrictMode>
     <App />
